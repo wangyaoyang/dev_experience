8c4f62cab05aa99be03aff003643942879a5916d
Yaoyang Wang <wangyaoyang@yahoo.com>
2023-02-05 15:53:07 -0500

    Added a bash script 'console_ft.sh' for uploading files without ethernet. Added a description for git server setup.
    Signed-off-by: Yaoyang Wang <wangyaoyang@yahoo.com>

---
+++ LInux/file_transfer_over_uart/console_ftp.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+# Author : Yaoyang(John) Wang
+# by 2023-02
+
+COLOR_RED="\e[31m"
+COLOR_GREEN="\e[32m"
+COLOR_YELLOW="\e[33m"
+COLOR_BLUE="\e[34m"
+COLOR_MAGENTA="\e[35m"
+COLOR_CYAN="\e[36m"
+COLOR_GRAY="\e[37m"
+COLOR_WHITE="\e[97m"
+COLOR_RESET="\e[0m"
+
+wait_key() {
+	if [ $# -gt 0 ]; then
+		sleep 5;
+		echo -e "$COLOR_RESET \n$@\n";
+	else
+		sleep 1;
+		echo -e "$COLOR_RESET \nPress any key to continue.\n";
+	fi
+	echo -e "$COLOR_GREEN";
+	while [ true ] ; do
+		read -t 30 -n 1
+		if [ $? = 0 ] ; then return; fi
+	done
+}
+
+install_lrz() {
+	UART_DEV=$1
+	SRC_FILE=lrz.ipk
+	MD5_SRC=`md5sum $SRC_FILE`
+	j=0;
+	echo -n -e "cd /tmp && rm lrz.txt" > $UART_DEV < $UART_DEV;
+	echo " " > $UART_DEV;
+	echo -n -e "echo \"$MD5_SRC\" > lrz_src.md5" > $UART_DEV; 
+	echo " " > $UART_DEV;
+	echo -n -e "echo \"" > $UART_DEV;
+	for i in $(cat lrz.txt);
+		do
+			j=$(($j+1));
+			echo -n -e "$i" > $UART_DEV;
+			echo -n -e " " > $UART_DEV;
+			if [ $(expr $j % 32) -eq 0 ]; then
+				echo -n -e "\" >> lrz.txt" > $UART_DEV;
+				echo " " > $UART_DEV;
+				echo -n -e "echo \"" > $UART_DEV;
+				echo -n -e "#"
+			fi;
+		done;
+	echo -n -e "\" >> lrz.txt" > $UART_DEV;
+	echo " " > $UART_DEV;
+	echo -e "$COLOR_GREEN \nConverting..."
+	echo -n -e "for i in \$(cat lrz.txt) ; do printf \"\x\$i\" ; done > lrz.ipk" > $UART_DEV;
+	echo " " > $UART_DEV;
+	cat $UART_DEV &
+	echo -n -e "sleep 3 && rm lrz.txt && md5sum lrz.ipk > lrz_dst.md5" > $UART_DEV;
+	echo " " > $UART_DEV;
+	echo -n -e "echo \"Verify hash code:\" && cat lrz_src.md5 && cat lrz_dst.md5" > $UART_DEV;
+	echo " " > $UART_DEV;
+	echo -n -e "if [ -z \"`cmp -l lrz_src.md5 lrz_dst.md5`\" ]; then echo \"Checksum OK!\" && opkg install lrz.ipk; " > $UART_DEV;
+	echo -n -e "else echo \"MD5 Failed!\"; fi" > $UART_DEV;
+	echo " " > $UART_DEV;
+	wait_key "Press ENTER after xyzmodem tool 'lrzsz' installed..."
+	killall cat
+}
+
+upload_file() {
+	UART_DEV=$1
+	SRC_FILE=$2
+	MD5_SRC=`md5sum $SRC_FILE`
+	echo -n -e "cd /tmp && rm $SRC_FILE" > $UART_DEV;
+	echo " " > $UART_DEV;
+	echo -n -e "echo \"$MD5_SRC\" > src.md5" > $UART_DEV;
+	echo " " > $UART_DEV;	
+	echo -n -e "lrz -Z" > $UART_DEV;
+	echo " " > $UART_DEV;
+	sz --zmodem $SRC_FILE > $UART_DEV < $UART_DEV;
+	cat $UART_DEV &
+	echo -n -e "md5sum $SRC_FILE > dst.md5" > $UART_DEV;
+	echo " " > $UART_DEV;
+	echo -n -e "echo \"Verify hash code:\" && cat src.md5 && cat dst.md5" > $UART_DEV;
+	echo " " > $UART_DEV;
+	echo -n -e "if [ -z \"`cmp -l src.md5 dst.md5`\" ]; then echo \"Checksum OK!\"; else echo \"MD5 Failed!\"; fi" > $UART_DEV;
+	echo " " > $UART_DEV;
+	wait_key
+	killall cat
+}
+
+file_transfer() {
+	if [ $# -eq 1 ]; then
+		echo -e "$COLOR_BLUE Installing xyz/modem tool over console..."
+		echo -e "$COLOR_GREEN Uploading lrz.ipk through $1..."
+		if [ -c $1 ]; then
+			install_lrz $@
+		else
+			echo -e "$COLOR_RED $1 is not a valid device name";
+		fi
+	elif [ $# -gt 1 ]; then
+		UART_DEV=$1
+		SRC_FILE=$2
+		
+		if [ -c $UART_DEV ]; then
+			if [ -f "$SRC_FILE" ]; then
+				echo -e "$COLOR_MAGENTA Uploading $SRC_FILE through $1..."
+				upload_file $@
+			else
+				#echo -e "$COLOR_RED The specified file '$2' is not exist."
+				cat $UART_DEV &
+				echo -e "$COLOR_GREEN$SRC_FILE$COLOR_RESET is not a file, Sending $COLOR_GREEN$SRC_FILE$COLOR_RESET as a commands...";
+				echo -n -e "${@:2}" > $UART_DEV;
+				echo " " > $UART_DEV;
+				sleep 3;
+				killall cat
+			fi
+		else
+			echo -e "$COLOR_RED $1 is not a valid device name";
+		fi
+	else
+		echo -e "$COLOR_GREEN Usage :"
+		echo -e "$COLOR_GREEN $0 DEV_NAME [FILE_NAME]"
+		echo -e "$COLOR_GREEN     DEV_NAME     UART device being used to transfer files. (e.g. /dev/ttyUSB0)."
+		echo -e "$COLOR_GREEN     FILE_NAME    Specify the file that need to be uploaded over the TTY console."
+		echo -e "$COLOR_GREEN                  If no file is specified, it will try to install 'lrzsz' on the target board."
+		echo -e "$COLOR_GREEN                  If specified file not exist, the file name be treated as a command to the console."
+	fi
+	echo -e "$COLOR_RESET"
+}
+
+file_transfer "$@"
+
